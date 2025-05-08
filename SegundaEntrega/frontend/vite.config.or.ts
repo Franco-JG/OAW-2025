0672beb7760ff23ea6172baa7b2e0bb3352913f3
John2Falcon <105178335+John2Falcon@users.noreply.github.com>
2025-05-07 23:05:52 -0600

    Agregada carpeta SegundaEntrega con optimizaciones

--- SegundaEntrega/frontend/vite.config.or.ts
+++ SegundaEntrega/frontend/vite.config.or.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/dist_original/", // <-- Este es el cambio clave
+  base: "/version_original/", // <-- Este es el cambio clave
   build: {
-    outDir: 'dist_original',
+    outDir: 'version_original',
     minify: false,
     sourcemap: true
   }
